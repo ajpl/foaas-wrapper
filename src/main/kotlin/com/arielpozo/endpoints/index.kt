3032719f068eac029efe970654f829d424bad193
ajpl <ajpl@users.noreply.github.com>
2022-05-02 03:07:58 -0300

    Add basic logging

--- src/main/kotlin/com/arielpozo/endpoints/index.kt
+++ src/main/kotlin/com/arielpozo/endpoints/index.kt
@@ -2,6 +2,7 @@ package com.arielpozo.endpoints
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.plugins.origin
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -10,6 +11,7 @@ fun Application.index() {
 
     routing {
         get("/") {
+            call.application.environment.log.info("[${call.request.origin.remoteHost}] - Accessed to \"/\"")
             call.respondText("Hello World!")
         }
     }
